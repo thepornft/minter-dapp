f4c98a79ecfc39be2ae5af9c3760b89c3bf0f5ed
Nick <thepornft@gmail.com>
2022-04-11 19:19:07 +0300

    colors

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,22 +1,19 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0x01bcfF9F1581504b020af2b03692BC8043180F94"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
-const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
+const welcome_h1 = "GM is our native language, welcome. Roadmap: GM - GN.";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
-const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
-const h1_presale_mint = "Pre-Sale Minting Open!!";
-const h1_public_mint = "Public Minting Open!!";
-const h2_presale_coming_soon = "Pre-Sale Minting Countdown";
-const h2_presale_mint = "Public Minting Countdown";
-const p_presale_coming_soon = "We are working hard to launch the NFT Collection. Stay tuned for updates!";
-const p_presale_mint_not_whitelisted = "You are not whitelisted for the pre-sale.. 😢";
-const p_presale_mint_whitelisted = "You're on the whitelist for the pre-sale! 🎉";
+const welcome_p = 'The GMfam NFT Project is a decentralized, open-source project... wtf? GM is GM, it costs 0 ETH to say GM. Say GM to us on <a href="https://twitter.com/GMfam_smile"  target="_blank" rel="noopener noreferrer">GMfam</a> Twitter.';
+const h1_presale_coming_soon = "GMfam NFTs cost 0 ETH. Mint free";
+const h1_presale_mint = "Pre-Sale? No, mint some.";
+const h1_public_mint = "-";
+const h2_presale_coming_soon = "Free mint countdown";
+const h2_presale_mint = "-";
+const p_presale_coming_soon = "We are working for the final of the Roadmap GNfam :)";
 const p_presale_mint_already_minted = "You've already claimed your whitelist mint. Thank you! 🎉";
 const p_public_mint = "No whitelist needed. Public minting is now open! 🎉";
-const button_presale_coming_soon = "Get on the Whitelist";
-const button_presale_mint_whitelisted = "Mint Your Special NFT";
-const button_presale_mint_not_whitelisted = "Get on the Whitelist";
+const button_presale_coming_soon = "Get on the Whitelist? No, we are free mint fam.";
+const button_presale_mint_whitelisted = "Mint Your free GMfam NFT";
 const button_presale_already_minted = "Join The Community";
 const button_public_mint = "Mint Your NFT";
 const mint_failed = "Minting failed. 😢 Please try again.";
